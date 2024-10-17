642156b93b3e4a9b0db8fb2efb90bff7c4670211
LKrAxViCt0R <vickylakra10@gmail.com>
2024-10-17 23:24:34 +0530

    Added debounced search and movies fetching

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+
 import { Search } from "@bigbinary/neeto-icons";
 import { Input } from "@bigbinary/neetoui";
+import moviesApi from "apis/movies";
+import useDebounce from "hooks/useDebounce";
 
-export const SearchBar = () => (
-  <div className="pr-20">
-    <Input placeholder="Search" prefix={<Search />} type="search" />
-  </div>
-);
+export const SearchBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const debouncedSearchKey = useDebounce(searchInput);
+
+  const fetchMovies = async () => {
+    const response = await moviesApi.show(debouncedSearchKey);
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, [debouncedSearchKey]);
+
+  return (
+    <div className="pr-20">
+      <Input
+        placeholder="Search"
+        prefix={<Search />}
+        type="search"
+        value={searchInput}
+        onChange={e => setSearchInput(e.target.value)}
+      />
+    </div>
+  );
+};
