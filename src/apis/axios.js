e6bb1468a7d1dda7401793864636ccfdfd9ad1bb
LKrAxViCt0R <vickylakra10@gmail.com>
2024-10-17 22:47:45 +0530

    Setup axios

---
+++ src/apis/axios.js
@@ -0,0 +1,43 @@
+import { Toastr } from "@bigbinary/neetoui";
+import axios from "axios";
+import { keysToCamelCase } from "neetocist";
+
+const setHttpHeaders = () => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+};
+
+const shouldShowToastr = response =>
+  typeof response === "object" && response?.noticeCode;
+
+const showSuccessToastr = response => {
+  if (shouldShowToastr(response.data)) Toastr.success(response.data);
+};
+
+// const showErrorToastr = error => {
+//     if (error.message === )
+// }
+
+const transformResponseKeysToCamelCase = response => {
+  if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const responseInterceptors = () => {
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+      showSuccessToastr(response);
+
+      return response.data;
+    },
+    error => Promise.reject(error)
+  );
+};
+
+export default function initializeAxios() {
+  axios.defaults.baseURL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+  setHttpHeaders();
+  responseInterceptors();
+}
