17bd0635463391524a4e7b810e55c01661b3dcd1
LKrAxViCt0R <vickylakra10@gmail.com>
2024-11-08 01:43:08 +0530

    Storing favorites in local storage

--- src/stores/useFavoritesStore.js
+++ src/stores/useFavoritesStore.js
@@ -1,20 +1,21 @@
+import { getFromLocalStorage, setToLocalStorage } from "utils/storage";
 import { create } from "zustand";
 
+import { FAVORITE_LOCAL_STORAGE_KEY } from "./constants";
+
 const useFavoritesStore = create(set => ({
-  favorites: [],
+  favorites: getFromLocalStorage(FAVORITE_LOCAL_STORAGE_KEY) || [],
   toggleFavorite: movie =>
     set(state => {
       const movieExists = state.favorites.some(m => m.id === movie.id);
 
-      if (movieExists) {
-        return {
-          favorites: state.favorites.filter(m => m.id !== movie.id),
-        };
-      }
+      const updatedFavorites = movieExists
+        ? state.favorites.filter(m => m.id !== movie.id)
+        : [...state.favorites, movie];
 
-      return {
-        favorites: [...state.favorites, movie],
-      };
+      setToLocalStorage(FAVORITE_LOCAL_STORAGE_KEY, updatedFavorites);
+
+      return { favorites: updatedFavorites };
     }),
 }));
 
