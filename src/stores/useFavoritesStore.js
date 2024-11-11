b36b966afa64913e12bbec9fa9e8c176e4919eb5
LKrAxViCt0R <vickylakra10@gmail.com>
2024-11-11 21:54:47 +0530

    Implemented favorites and view history to be persisted

--- src/stores/useFavoritesStore.js
+++ src/stores/useFavoritesStore.js
@@ -1,23 +1,27 @@
-import { findById } from "neetocist";
-import { getFromLocalStorage, setToLocalStorage } from "utils/storage";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-import { FAVORITE_LOCAL_STORAGE_KEY } from "./constants";
+const useFavoritesStore = create(
+  persist(
+    set => ({
+      favorites: [],
+      toggleFavorite: movie =>
+        set(state => {
+          const movieExists = state.favorites.some(m => m.id === movie.id);
 
-const useFavoritesStore = create(set => ({
-  favorites: getFromLocalStorage(FAVORITE_LOCAL_STORAGE_KEY) || [],
-  toggleFavorite: movie =>
-    set(state => {
-      const movieExists = findById(movie.id, state.favorites);
+          if (movieExists) {
+            return {
+              favorites: state.favorites.filter(m => m.id !== movie.id),
+            };
+          }
 
-      const updatedFavorites = movieExists
-        ? state.favorites.filter(m => m.id !== movie.id)
-        : [...state.favorites, movie];
-
-      setToLocalStorage(FAVORITE_LOCAL_STORAGE_KEY, updatedFavorites);
-
-      return { favorites: updatedFavorites };
+          return {
+            favorites: [...state.favorites, movie],
+          };
+        }),
     }),
-}));
+    { name: "favorites-store" }
+  )
+);
 
 export default useFavoritesStore;
--- src/stores/useViewHistoryStore.js
+++ src/stores/useViewHistoryStore.js
@@ -1,46 +1,41 @@
-import { findById } from "neetocist";
-import { getFromLocalStorage, setToLocalStorage } from "utils/storage";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-import { HISTORY_LOCAL_STORAGE_KEY } from "./constants";
+const useViewHistoryStore = create(
+  persist(
+    set => ({
+      viewHistory: [],
+      addToHistory: movie =>
+        set(state => {
+          const movieExists = state.viewHistory.some(m => m.id === movie.id);
 
-const useViewHistoryStore = create(set => ({
-  viewHistory: getFromLocalStorage(HISTORY_LOCAL_STORAGE_KEY) || [],
-  addToHistory: movie =>
-    set(state => {
-      const movieExists = findById(movie.id, state.favorites);
+          if (movieExists) {
+            return {
+              viewHistory: state.viewHistory.map(m => ({
+                ...m,
+                isRecent: m.id === movie.id,
+              })),
+            };
+          }
 
-      const updatedHistory = movieExists
-        ? state.viewHistory.map(m => ({
-            ...m,
-            isRecent: m.id === movie.id,
-          }))
-        : [
-            { ...movie, isRecent: true },
-            ...state.viewHistory.map(m => ({ ...m, isRecent: false })),
-          ];
-
-      setToLocalStorage(HISTORY_LOCAL_STORAGE_KEY, updatedHistory);
-
-      return { viewHistory: updatedHistory };
+          return {
+            viewHistory: [
+              { ...movie, isRecent: true },
+              ...state.viewHistory.map(m => ({ ...m, isRecent: false })),
+            ],
+          };
+        }),
+      removeFromHistory: movieId =>
+        set(state => ({
+          viewHistory: state.viewHistory.filter(movie => movie.id !== movieId),
+        })),
+      clearHistory: () =>
+        set(() => ({
+          viewHistory: [],
+        })),
     }),
-
-  removeFromHistory: movieId =>
-    set(state => {
-      const updatedHistory = state.viewHistory.filter(
-        movie => movie.id !== movieId
-      );
-      setToLocalStorage(HISTORY_LOCAL_STORAGE_KEY, updatedHistory);
-
-      return { viewHistory: updatedHistory };
-    }),
-
-  clearHistory: () =>
-    set(() => {
-      setToLocalStorage(HISTORY_LOCAL_STORAGE_KEY, []);
-
-      return { viewHistory: [] };
-    }),
-}));
+    { name: "view-history-store" }
+  )
+);
 
 export default useViewHistoryStore;
